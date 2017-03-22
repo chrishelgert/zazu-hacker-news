ecad396c0c352ec54f7f027f11d8e68ced662a52
Chris Helgert <chrishelgert@users.noreply.github.com>
2017-03-22 17:30:58 +0100

    add id´s for better ranking (#3)

--- src/hacker-news.js
+++ src/hacker-news.js
@@ -1,13 +1,14 @@
 const got = require('got')
 
-module.exports.load = () => (
+module.exports.load = () =>
   got('http://node-hnapi.herokuapp.com/news', { json: true })
-    .then(response => response.body.map(article => ({
-      title: article.title,
-      value: article.url,
-    })))
+    .then(response =>
+      response.body.map(article => ({
+        id: `hn-${article.title}`,
+        title: article.title,
+        value: article.url,
+      })))
     .catch((error) => {
       // eslint-disable-next-line no-console
       console.error(error.response.body)
     })
-)
